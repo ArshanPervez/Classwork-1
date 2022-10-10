afa7e8b2cf5bc779b4c41eab9c5d168a4a63a4f0
ArshanPervez <113149145+ArshanPervez@users.noreply.github.com>
2022-10-10 15:03:14 -0400

    Finished CW

--- array1.java
+++ array1.java
@@ -1,4 +1,5 @@
 public class array1{
+
   public static int[] returnCopy(int[]ary){
     int len = ary.length;
     int[] aryTwo = new int[len];
@@ -8,6 +9,7 @@ public class array1{
     }
     return aryTwo;
   }
+
   public static String returnCopyString(int[] a){
     String oof = "";
     a = returnCopy(a);
@@ -16,6 +18,7 @@ public class array1{
     }
     return oof;
   }
+
   public static String returnCopyStringAgain(int[] a){
     String oof = "";
     for (int i = 0; i < a.length; i++){
@@ -23,4 +26,20 @@ public class array1{
     }
     return oof;
   }
+
+  //End of First Testing
+
+  public static int[] concatArray(int[]ary1,int[]ary2){
+    int lol = ary1.length;
+    int len = ary1.length + ary2.length;
+    int[] ary3 = new int[len];
+    for (int x = 0; x < ary1.length; x++){
+        ary3[x] = ary1[x];
+      }
+    for (int y = 0; y < ary2.length; y++){
+      ary3[lol + y] = ary2[y];
+    }
+    return ary3;
+  }
+
 }
--- test.java
+++ test.java
@@ -5,6 +5,9 @@ public class test{
     System.out.println("The cloned string address is" + array1.returnCopy(lol));
     System.out.println("The original string is " + array1.returnCopyStringAgain(lol));
     System.out.println("The cloned string is " + array1.returnCopyString(lol));
-    //System.out.println("The o")
+    //End of First Testing
+    int[] oof = {5, 6, 7, 8, 9};
+
+    System.out.println("The concatenated string of {1,2,3} and {5,6,7,8,9} is:" + array1.returnCopyStringAgain(array1.concatArray(lol,oof)) );
   }
 }
